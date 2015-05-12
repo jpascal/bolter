9efab0073681b085700181aca0f0ad90c032902a
Evgeniy Shurmin <eshurmin@gmail.com>
2015-05-12 12:24:49 +0300

    fix sorting with nil value

--- lib/bolter/sortable.rb
+++ lib/bolter/sortable.rb
@@ -3,14 +3,14 @@ module Bolter
     extend ActiveSupport::Concern
     included do
       def self.sorting(sorting = nil)
-        return self unless sorting
+        return self.order({}) unless sorting
         field, direction = sorting.split(':',2)
         if self.attribute_names.include? field.to_s
           if %W[asc desc].include? direction
-            return self.order(Hash[field, direction])
+            return self.reorder(Hash[field, direction])
           end
         else
-          self
+          self.order({})
         end
       end
     end
