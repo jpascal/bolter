ef03fa3f3599a547c539c60011ce7b034f7b33e7
Evgeniy Shurmin <eshurmin@gmail.com>
2016-06-05 20:42:27 +0300

    fix

--- lib/bolter/searchable.rb
+++ lib/bolter/searchable.rb
@@ -4,7 +4,7 @@ module Bolter
     included do
       cattr_reader :used_as_filters
       def self.search(params = {}, options = {})
-        result = self.current_scope
+        result = self.current_scope || self
         # Prepare params to search
         search = (params || {}).clone.reject{|_,value| value.to_s.empty? }
         # Apply filters
